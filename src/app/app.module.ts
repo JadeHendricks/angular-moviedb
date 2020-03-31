8676fbfaa08082c93d8d69ae13df5ba11fdab10e
jade <hendricksjade@gmail.com>
2020-03-31 16:59:07 +0200

    components generated and added to the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { TopNavigationComponent } from './components/layouts/top-navigation/top-navigation.component';
+import { SideNavigationComponent } from './components/layouts/side-navigation/side-navigation.component';
+import { MainContentComponent } from './components/layouts/main-content/main-content.component';
+import { ModalComponent } from './components/layouts/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopNavigationComponent,
+    SideNavigationComponent,
+    MainContentComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
