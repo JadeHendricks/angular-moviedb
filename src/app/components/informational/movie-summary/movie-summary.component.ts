dc4e204541053181b4b141076e2a48276bd35433
Jade <hendricksjade@gmail.com>
2022-08-14 16:39:00 +0200

    refactoring the movie summary component

--- src/app/components/informational/movie-summary/movie-summary.component.ts
+++ src/app/components/informational/movie-summary/movie-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ContentService } from 'src/app/services/content/content.service';
 import { BaseService } from '../../../services/base/base.service';
@@ -7,67 +7,95 @@ import { Cast } from 'src/app/models/Cast';
 import { Content } from 'src/app/models/Content';
 import { Review } from 'src/app/models/Review';
 import { Video } from 'src/app/models/Video';
-import { Reviews } from 'src/app/models/Reviews';
-import { Videos } from 'src/app/models/Videos';
 import { Contents } from 'src/app/models/Contents';
-import { Credits } from 'src/app/models/Credits';
 import { Genre } from 'src/app/models/Genre';
+import { forkJoin, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movie-summary',
   templateUrl: './movie-summary.component.html'
 })
-export class MovieSummaryComponent implements OnInit {
+export class MovieSummaryComponent implements OnInit, OnDestroy {
   
-  siteState: string = "";
-  id: string;
-  content: Content;
-  reviews: Review[];
-  cast: Cast[];
-  videos: Video[];
-  similarContent: Content[];
-  trailerKey: string;
-  genres: Genre[];
+  private siteState: string = "";
+  private trailerKey: string = "";
+  
+  public id: string = "";
+  public backgroundImage: string = "";
+  public content: Content;
+  public reviews: Review[];
+  public cast: Cast[];
+  public videos: Video[];
+  public similarContent: Content[];
+  public genres: Genre[];
+
+  private contentStateSubscription: Subscription;
+  private getGenresSubscription: Subscription;
+  private getTrailerSubscription: Subscription;
 
   constructor(
     private route: ActivatedRoute, 
     private contentService: ContentService, 
     private baseService: BaseService,
-    private sanitizer: DomSanitizer) { }
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get("id");
-    this.baseService.contentState.subscribe((value: string) => {
-      this.siteState = value;
-      this.siteState ? this.getData() : false;
-    })
+    this.registerSubscriptions();
   }
 
-  setGenresArray(content: Content): void {
-    this.contentService.getGenres().subscribe((value: Contents) => {
-      this.genres = value.genres.filter((genreOne: Genre) => content.genres.find(genreTwo => genreOne.id === genreTwo.id)).slice(0, 2);
+  private registerSubscriptions(): void {
+    this.getContentState();
+  }
+
+  private getContentState(): void {
+    this.contentStateSubscription = this.baseService.contentState.subscribe((value: string) => {
+      if (value) {
+        this.siteState = value;
+        this.siteState ? this.getData() : false;
+      }
     });
   }
 
-  getData(): void {
-    this.contentService.getContent(this.id).subscribe((movie: Content) => {
-      this.content = movie;
+  private setGenresArray(content: Content): void {
+    this.getGenresSubscription = this.contentService.getGenres().subscribe((value: Contents) => {
+      if (value && Object.keys(value).length) {
+        this.genres = value.genres.filter((genreOne: Genre) => content.genres.find(genreTwo => genreOne.id === genreTwo.id)).slice(0, 2);
+      }
+    });
+  }
+
+  private getData(): void {
+    forkJoin([
+      this.contentService.getContent(this.id),
+      this.contentService.getReviews(this.id),
+      this.contentService.getCast(this.id),
+      this.contentService.getVideos(this.id),
+      this.contentService.getSimilarContent(this.id)
+    ]).subscribe(([content, reviews, cast, videos, similarContent]) => {
+      this.setMovieSummaryValues(content, reviews, cast, videos, similarContent);
       this.setGenresArray(this.content);
+      this.getBackgroundImageUrl(this.content);
     });
-    this.contentService.getReviews(this.id).subscribe((review: Reviews) => this.reviews = review.results);
-    this.contentService.getCast(this.id).subscribe((cast: Credits) => this.cast = cast.cast.slice(0, 12));
-    this.contentService.getVideos(this.id).subscribe((video: Videos) => this.videos = video.results.slice(0, 4));
-    this.contentService.getSimilarContent(this.id).subscribe((movie: Contents) => this.similarContent = movie.results);
   }
 
-  backgroundImageUrl(): string {
+  private setMovieSummaryValues(content, reviews, cast, videos, similarContent): void {
+    this.reviews = reviews.results;
+    this.cast = cast.cast.slice(0, 12);
+    this.videos = videos.results.slice(0, 4);
+    this.similarContent = similarContent.results;
+    this.content = content;
+  }
+
+  private getBackgroundImageUrl(content: Content): void {
     if (this.content) {
-      return `url(https://image.tmdb.org/t/p/original/${this.content?.backdrop_path})`;
+      this.backgroundImage = `url(https://image.tmdb.org/t/p/original/${content?.backdrop_path})`
     }
   }
 
-  getTrailer(id: number): void {
-    this.contentService.getTrailer(id).subscribe(trailer => {
+  public getTrailer(id: number): void {
+    this.getTrailerSubscription = this.contentService.getTrailer(id).subscribe(trailer => {
       this.trailerKey = trailer.results[0].key
       if (this.trailerKey) {
         window.open(
@@ -78,7 +106,15 @@ export class MovieSummaryComponent implements OnInit {
     });
   }
 
-  getEmbedUrl(video: any): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${video?.key}`);
+  public getEmbedUrl(video: any): SafeResourceUrl {
+    if (video) {
+      return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${video?.key}`);
+    } 
+  }
+
+  ngOnDestroy(): void {
+    if (this.contentStateSubscription) this.contentStateSubscription.unsubscribe();
+    if (this.getGenresSubscription) this.getGenresSubscription.unsubscribe();
+    if (this.getTrailerSubscription) this.getTrailerSubscription.unsubscribe();
   }
 }
