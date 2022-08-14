a8538c917eba509ee9cdccc1b32b3eee50a29f31
Jade <hendricksjade@gmail.com>
2022-08-14 15:16:23 +0200

    refactoring the header component

--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit, Input, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnDestroy } from '@angular/core';
 import { ContentService } from '../../../services/content/content.service'
 import { Content } from 'src/app/models/Content';
 import { Contents } from 'src/app/models/Contents';
 import { Videos } from 'src/app/models/Videos';
 import { Genre } from 'src/app/models/Genre';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
-  
+export class HeaderComponent implements OnInit, OnDestroy {
+
   @Input() content: Content;
-  trailerKey: string;
-  genres: Genre[];
+  private trailerKey: string;
+  public genres: Genre[];
+  public backgroundImage: string = '';
 
-  constructor(private contentService: ContentService) { }
+  private getTrailerSubscription: Subscription;
+  private getGenresSubscription: Subscription;
 
-  ngOnInit(): void { }
+  constructor(
+    private contentService: ContentService
+  ) { }
+
+  ngOnInit(): void {
+    if (this.content) {
+      this.getBackgroundImageUrl(this.content.backdrop_path);
+    }
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes.content.currentValue) { 
+    if (changes.content.currentValue) {
       this.setGenresArray(changes.content.currentValue);
     }
   }
 
-  getTrailer(id: number): void {
-    this.contentService.getTrailer(id).subscribe((trailer: Videos) => {
-      this.trailerKey = trailer.results[0].key
+  public getTrailer(id: number): void {
+    this.getTrailerSubscription = this.contentService.getTrailer(id).subscribe((trailer: Videos) => {
+      this.trailerKey = trailer.results[0].key;
       if (this.trailerKey) {
         window.open(
           `https://www.youtube.com/watch?v=${this.trailerKey}`,
@@ -37,15 +48,20 @@ export class HeaderComponent implements OnInit {
     });
   }
 
-  setGenresArray(content: Content): void {
-    this.contentService.getGenres().subscribe((value: Contents) => {
+  private setGenresArray(content: Content): void {
+    this.getGenresSubscription = this.contentService.getGenres().subscribe((value: Contents) => {
       this.genres = value.genres.filter((element: Genre) => content.genre_ids.includes(element.id)).slice(0, 2);
     });
   }
 
-  backgroundImageUrl(): string {
+  private getBackgroundImageUrl(contentBackDrop: string): void {
     if (this.content) {
-      return `url(https://image.tmdb.org/t/p/original/${this.content?.backdrop_path})`;
+      this.backgroundImage = `url(https://image.tmdb.org/t/p/original/${contentBackDrop})`;
     }
- }
+  }
+
+  ngOnDestroy(): void {
+    if (this.getTrailerSubscription) this.getTrailerSubscription.unsubscribe();
+    if (this.getGenresSubscription) this.getGenresSubscription.unsubscribe();
+  }
 }
