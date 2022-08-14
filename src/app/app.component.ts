2f089f4dd58378294f260d20856ab33c679d1de5
Jade <hendricksjade@gmail.com>
2022-08-14 15:47:24 +0200

    refactoring the modal and app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BaseService } from './services/base/base.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  showModal: boolean;
-  modalState: string;
+  public showModal: boolean;
+  public modalState: string;
   
-  constructor(private baseService: BaseService) { }
+  constructor(
+    private baseService: BaseService
+  ) { }
 
   ngOnInit(): void {
     this.baseService.modalState.subscribe((value: any) => {
-      this.showModal = value.hidden;
-      this.modalState = value.state;
+      if (value && Object.keys(value).length) {
+        this.showModal = value.hidden;
+        this.modalState = value.state;
+      } 
     });
   }
 }
--- src/app/components/layouts/modal/modal.component.ts
+++ src/app/components/layouts/modal/modal.component.ts
@@ -8,34 +8,41 @@ import { BaseService } from '../../../services/base/base.service';
 export class ModalComponent implements OnInit {
   @Input() showState: string;
 
-  constructor(private baseService: BaseService) { }
+  constructor(
+    private baseService: BaseService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  modalStateSwap(event: any): void {
-    const stateValue = event.srcElement.innerText.toLowerCase();
-    this.baseService.modalStateSwap({ hidden: false, state: stateValue });
+  public modalStateSwap(event: any): void {
+    if (event && event.srcElement) {
+      const stateValue = event.srcElement.innerText.toLowerCase();
+      this.baseService.modalStateSwap({ hidden: false, state: stateValue });
+    }
   }
 
-  hideModal(event: any): boolean {
-    if (event.target.classList.contains("overlay")) {
-      this.baseService.modalStateSwap({ hidden: true, state: "" });
+  public hideModal(event: any): boolean {
+    if (event && event.target.classList) {
+      if (event.target.classList.contains("overlay")) {
+        this.baseService.modalStateSwap({ hidden: true, state: "" });
+      }
     }
     return false;
   }
 
-  showPassword(event: any): void {
-    const passwordInput = event.target.parentNode.previousSibling as HTMLInputElement;
-    const visibleIcon = event.target as HTMLInputElement;
+  public showPassword(event: any): void {
+    if (event && event.target && event.target.parentNode && event.target.parentNode.previousSibling) {
+      const passwordInput = event.target.parentNode.previousSibling as HTMLInputElement;
+      const visibleIcon = event.target as HTMLInputElement;
 
-    if (passwordInput.getAttribute("type") === "password") {
-      passwordInput.setAttribute("type", "text");
-      visibleIcon.setAttribute("xlink:href", "assets/sprite.svg#icon-eye-blocked");
-    } else {
-      passwordInput.setAttribute("type", "password");
-      visibleIcon.setAttribute("xlink:href", "assets/sprite.svg#icon-eye");
+      if (passwordInput.getAttribute("type") === "password") {
+        passwordInput.setAttribute("type", "text");
+        visibleIcon.setAttribute("xlink:href", "assets/sprite.svg#icon-eye-blocked");
+      } else {
+        passwordInput.setAttribute("type", "password");
+        visibleIcon.setAttribute("xlink:href", "assets/sprite.svg#icon-eye");
+      }
     }
   }
-  
 }
